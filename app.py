38dc2e2a5ae3c51e2bb4763e6ebef74bce7c0253
aroraaman25 <amanaroraec1010@gmail.com>
2024-05-19 02:32:50 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
+    #return ('Hello, World!')
+
+@app.route('/products')
+def products():
+    return('This is an e-commerce website')
+
+if __name__== "__main__":
+    app.run(debug=True, port=8000)
